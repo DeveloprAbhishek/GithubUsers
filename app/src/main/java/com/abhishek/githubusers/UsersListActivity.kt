2c84cbc8d408ad06f53c502692eac76ab685b050
MeeshoAbhishek <abhishek.khichi@meesho.com>
2025-08-22 19:16:41 +0530

    Setup Project

---
+++ app/src/main/java/com/abhishek/githubusers/UsersListActivity.kt
@@ -0,0 +1,13 @@
+package com.abhishek.githubusers
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class UsersListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
